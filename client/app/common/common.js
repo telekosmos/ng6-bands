5308ec459fef2fbb20b20ccdffe12a499590ce2d
telekosmos <telekosmos@ymail.com>
2016-06-21 19:30:39 +0200

    Cleansing

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -2,18 +2,12 @@
 
 import angular from 'angular';
 import Navbar from './navbar/navbar';
-import Hero from './hero/hero';
-import User from './user/user';
 
-import NotImplemented from './notimplemented/notimplemented';
 import LinksList from './linkslist/linkslist';
 
 let commonModule = angular.module('app.common', [
 	Navbar.name,
-	Hero.name,
-	User.name,
-  NotImplemented.name,
-  LinksList.name
+	LinksList.name
 ]);
 
 export default commonModule;
